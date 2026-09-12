d2471b86b0eb41a9c85ca8e9f29462454902325d
Adrian Crossley <acecool11gaming@gmail.com>
2026-09-12 18:02:47 -0400

    moved classes around and atempted to compile it with some isshues

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from class_mon.py import mon
-from class_data_class.py import data_class
-from class_move.py import move
-from class_spcies.py import species
-
-
+from mon.class_mon import mon
+from mon.class_move import move
+from mon.class_species import species
+from mon.class_data_class import data_class
 
 def main():
-    data = data_class(r".\species.csv",r".\move.csv")
-
+    pokeydex = data_class(r".\data\species.csv",r".\data\move.csv")
+    mon1 = mon()
+    mon1.set_species(pokeydex.get_species["1"])
+    print(mon1.get_attack)
 
 
 
@@ -21,3 +21,14 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+'''
+def apply_heal(user, target, value):   target.current_hp += value
+def apply_damage(user, target, value): target.current_hp -= value
+
+EFFECTS = {"heal": apply_heal, "damage": apply_damage, "corrupt": apply_corrupt}
+
+def use_move(move, user, target):
+    EFFECTS[move.ability](user, target, move.value)
+'''
