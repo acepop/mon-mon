6a0f0ab40ebe47ce44abfab51f6ef75f0bc2f824
acepop <58615622+acepop@users.noreply.github.com>
2026-09-17 08:13:39 -0400

    more bs bupdats to the testing file

--- test.py
+++ test.py
@@ -1,27 +1,18 @@
 import json
 # relitive paths
-pre_path = (str(__file__)).replace("test.py","")
 
+
+
+
+pre_path = (str(__file__)).replace("test.py","")
 species_file = f"{pre_path}{r"data/species.json"}"
 move_file = f"{pre_path}{r"data/move.json"}"
 
+with open(species_file, "r") as f: # opens the file in read mode
+    mon_list = json.load(f)  # reads all of the data to a list
 
-
-
-
-
-with open(species_file, "r") as f: # with the listed file
-    data = json.load(f)  # reads all of the data to a string
-    dic = data[0]
-    for key, value in dic.items():
-        print(f"{key} : {value}")
-
-# trys to import file values as move_content and species_content
 with open(move_file) as f: # with the listed file
-    data =  json.load(f)  # reads all of the data to a string
-    dic = data[0]
-    for key, value in dic.items():
-        print(f"{key} : {value}")
+    move_list =  json.load(f)  # reads all of the data to a string
 
 def import_formater(atxt_file_import): 
     # changes the imported data into usibal data
@@ -29,18 +20,6 @@ def import_formater(atxt_file_import):
     # data in the atxt fils are specifyed with #s=data # is the deliminator
     # s is the string data type, = is another dleminator, there are no "" or 
 
-
-
-
-
-    return out_list 
-
-formated_move_content = import_formater(move_content)
-for m in formated_move_content:
-    print(f"{m} ##")
-    for i in m:
-        print(f"{i} ==")
-    #self.moves_list[move[0]] = move
 
 for s_list in species_content:
     self.species_list[s_list[0]] = species(s_list)
